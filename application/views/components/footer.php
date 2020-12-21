dca65115f8b6b40e99c8e6fdcf6ca3b74f879a9b
hoanglv <hoang.lv173128@sis.hust.edu.vn>
2020-12-21 12:38:03 +0700

    update

--- application/views/components/footer.php
+++ application/views/components/footer.php
@@ -150,16 +150,16 @@ if (!$conn) {
         </div>
         <div class="footer-list-film">
             <div class="footer-link">
-                <h3 class="footer-link-head">Phim Hay</h3><a href="phim-le/" title="Phim lẻ mới">Phim lẻ
-                    mới</a><a href="phim-bo/" title="Phim bộ mới">Phim bộ mới</a><a href="phim-chieu-rap/" title="Phim chiếu rạp 2013">Phim chiếu rạp</a><a href="phim-kinh-dien/" title="Phim kinh điển hay">Phim kinh điển</a><a href="trailer/" title="Trailer phim sắp chiếu">Phim sắp chiếu</a>
+                <h3 class="footer-link-head">Phim Hay</h3><a href="javascript:void(0);" title="Phim lẻ mới">Phim lẻ
+                    mới</a><a href="javascript:void(0);" title="Phim bộ mới">Phim bộ mới</a><a href="javascript:void(0);" title="Phim chiếu rạp 2013">Phim chiếu rạp</a><a href="javascript:void(0);" title="Phim kinh điển hay">Phim kinh điển</a><a href="javascript:void(0);" title="Trailer phim sắp chiếu">Phim sắp chiếu</a>
             </div>
             <div class="footer-link">
-                <h3 class="footer-link-head">Phim Lẻ</h3><a href="the-loai/phim-hanh-dong/">Phim hành động</a><a href="the-loai/phim-co-trang/">Phim kiếm hiệp</a><a href="the-loai/phim-kinh-di/">Phim kinh
-                    dị</a><a href="the-loai/phim-vien-tuong/">Phim viễn tưởng</a><a href="the-loai/phim-hoat-hinh/">Phim hoạt hình</a>
+                <h3 class="footer-link-head">Phim Lẻ</h3><a href="javascript:void(0);">Phim hành động</a><a href="javascript:void(0);">Phim kiếm hiệp</a><a href="javascript:void(0);">Phim kinh
+                    dị</a><a href="javascript:void(0);">Phim viễn tưởng</a><a href="javascript:void(0);">Phim hoạt hình</a>
             </div>
             <div class="footer-link">
-                <h3 class="footer-link-head">Phim Bộ</h3><a href="phim-bo/kr/" title="Phim bộ hàn quốc mới">Phim bộ
-                    Hàn Quốc</a><a href="phim-bo/cn/" title="Phim bộ Trung Quốc mới">Phim bộ Trung Quốc</a><a href="phim-bo/us/" title="Phim bộ của Mỹ mới">Phim bộ Mỹ</a><a href="phim-bo/vn/" title="Phim bộ Việt Nam mới">Phim bộ Việt Nam</a><a href="phim-bo/hk/" title="Phim bộ Hồng Kông mới">Phim bộ Hồng Kông</a>
+                <h3 class="footer-link-head">Phim Bộ</h3><a href="javascript:void(0);" title="Phim bộ hàn quốc mới">Phim bộ
+                    Hàn Quốc</a><a href="javascript:void(0);" title="Phim bộ Trung Quốc mới">Phim bộ Trung Quốc</a><a href="javascript:void(0);" title="Phim bộ của Mỹ mới">Phim bộ Mỹ</a><a href="javascript:void(0);" title="Phim bộ Việt Nam mới">Phim bộ Việt Nam</a><a href="javascript:void(0);" title="Phim bộ Hồng Kông mới">Phim bộ Hồng Kông</a>
             </div>
         </div>
         <div class="footer-contact">
